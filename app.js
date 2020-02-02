2a8d8e6d2725cc8b35e988b4cd02839e65925646
manish120 <pmanish1200@gmail.com>
2020-02-02 10:37:18 +0530

    basic structure

---
+++ app.js
@@ -0,0 +1,19 @@
+const express =require("express")
+const bodyParser=require("body-parser")
+
+
+const app=express()
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('./publicc'))
+app.set('view engine','ejs')
+
+
+//index page
+app.get("/",(req,res)=>{  
+    res.render("home.ejs");
+})
+
+app.listen(3000,()=>{
+    console.log("its work")
+})
