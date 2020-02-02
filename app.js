52177bb515e86967c6de9af3bac51fd126ca0d43
manish120 <pmanish1200@gmail.com>
2020-02-02 23:08:16 +0530

    non

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser=require("body-parser")
 const app=express()
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
-app.use(express.static('./publicc'))
+app.use(express.static('./public'))
 app.set('view engine','ejs')
 
 
